a5033dcd17a42293f2c52fdcdf96922e2d824d73
bibekrj <29625141+bibekrj@users.noreply.github.com>
2026-03-27 14:04:06 -0400

    get method working

--- src/pages/Todos.jsx
+++ src/pages/Todos.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-const API_URL = "https://localhost:3000//api/admin/todos"
+const API_URL = "http://localhost:3000/api/admin/todos";
 
 // CONST API_URL2 = "api/admin/todos"
 
@@ -15,11 +15,13 @@ export default function Todos(){
                 setLoading(true);
                 setError("");
                 const response = await fetch(API_URL);
+                console.log(response)
                 if (!response.ok){
                     throw new Error("Failed to fetch todos");
                 }
                 const data = await response.json();
-                setTodos(data);
+                console.log(data.todos)
+                setTodos(data.todos);
             }catch(error){
                 setError(error.message || "Something went wrong");
             }finally{
@@ -29,6 +31,28 @@ export default function Todos(){
         fetchTodos();
     }, []);
     return(
+        <section>
+            <h2>Todos </h2>
+            {loading && <p>Loading todos... </p>}
+            {error && <p> Error: {error}</p>}
 
-    )
+            {!loading && !error && todos.length===0  && <p>No todos found</p>}
+
+            {!loading && !error && todos.length >0 && (
+                <ul>
+                    {todos.map((todo) =>(
+                        <li key={todo.task_id}>
+                            <strong>{todo.tasks}</strong> {" "}
+                            {todo.completed ? "(Completed)": "(Not Completed)"}
+                        </li>
+                    ))}
+                </ul>
+            )}
+            
+
+        </section>
+        
+
+
+    );
 }
